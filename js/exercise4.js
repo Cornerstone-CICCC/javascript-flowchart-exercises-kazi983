13b640cf32812cd800356be52c69aa0a7737295d
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-20 14:12:25 -0700

    refactor: simplify triangle check

--- js/exercise4.js
+++ js/exercise4.js
@@ -2,22 +2,8 @@ let a = 23;
 let b = 30;
 let c = 7;
 
-let max = a;
-let min1 = b;
-let min2 = c;
-
-if (b > max) {
-    min1 = max;
-    max = b;
-} 
-
-if (c > max) {
-    min2 = max;
-    max = c;
-} 
-
-if (min1 + min2 > max) {
-    console.log("Triangle is possible!")
+if ( a + b > c && a + c > b && b + c > a) {
+    console.log("Triangle is possible!");
 } else {
-    console.log("Triangle is not possible :(")
+    console.log("Triangle is not possible :(");
 }
