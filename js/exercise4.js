18d636400fcafde48ff0ab423ab95294fa2bbc15
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-20 12:17:06 -0700

    Added exercise4.js

---
+++ js/exercise4.js
@@ -0,0 +1,23 @@
+let a = 23;
+let b = 30;
+let c = 5;
+
+let max = a;
+let min1 = b;
+let min2 = c;
+
+if (b > max) {
+    min1 = max;
+    max = b;
+} 
+
+if (c > max) {
+    min2 = max;
+    max = c;
+} 
+
+if (min1 + min2 >= max) {
+    console.log("Triangle is possible!")
+} else {
+    console.log("Triangle is not possible :(")
+}
